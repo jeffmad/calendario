09af674feff00c78d57cc9dcb19e8297079adcc0
Jeff Madynski <jmadynski@expedia.com>
2015-11-27 14:04:56 -0800

    add unit tests for user service

--- src/calendario/user_service.clj
+++ src/calendario/user_service.clj
@@ -25,23 +25,23 @@
                      :connection-manager conn-mgr })
    (catch java.net.SocketTimeoutException ste
      (error ste (str "socket timeout reading " url))
-     (throw+ {:cause :service-unavailable :error {:reason (.getMessage ste)
-                                                  :data meta-data
-                                                  :url url}}
+     (throw+ {:cause :service-unavailable
+              :error {:reason (.getMessage ste)
+                      :data meta-data
+                      :url url}}
              "encountered socket timeout"))
    (catch java.net.ConnectException ce
      (error ce (str "connect exception to url " url))
-     (throw+ {:cause :service-unavailable :error {:reason (.getMessage ce)
-                                                  :data meta-data
-                                                  :url url}}
+     (throw+ {:cause :service-unavailable
+              :error {:reason (.getMessage ce)
+                      :data meta-data
+                      :url url}}
              "encountered connection exception"))
    (catch Object _
      (error (:throwable &throw-context) "unexpected error")
      (throw+))))
+
 (def exp-account-template "<?xml version=\"1.0\" encoding=\"utf-8\"?><usr:listTUIDsForExpAccountRequest xmlns:usr=\"urn:com:expedia:www:services:user:messages:v3\" siteID=\"1\"><usr:expUser emailAddress=\"%s\" /><usr:messageInfo enableTraceLog=\"false\" clientName=\"localhost\" transactionGUID=\"a2192179-d5b7-4234-918c-8f662aaaf545\"/></usr:listTUIDsForExpAccountRequest>")
-
-;<?xml version="1.0" encoding="utf-8" standalone="yes"?><listTUIDsForExpAccountResponse success="true" xmlns="urn:com:expedia:www:services:user:messages:v3"><expUser id="301078" emailAddress="jmadynski@expedia.com"/><expUserTUIDMapping tpid="1" tuid="5363093" singleUse="true" updateDate="2014-04-25T09:55:00.000-07:00"/><expUserTUIDMapping tpid="1" tuid="577015" singleUse="false" updateDate="2014-05-30T22:26:00.000-07:00"/><authRealmID>1</authRealmID></listTUIDsForExpAccountResponse>
-
 
 (defn- tuid-mapping [t]
   {:tpid (zx/xml1-> t (zx/attr :tpid))
@@ -60,6 +60,11 @@
 ;{:expuserid "301078", :email "jmadynski@expedia.com", :tuidmappings [{:tpid "1", :tuid "5363093", :single-use "true"} {:tpid "1", :tuid "577015", :single-use "false"}]}
 ; "https://userservicev3.integration.karmalab.net:56783"
 (defn get-user-by-email
+  "given an http client and email address, call user service to retrieve
+   the siteid / tuid combinations that this email address has. If there
+   is a problem messaging the user service, an exception is thrown. If the
+   xml response does not indicate success = true, nil is returned. If the http
+   status of the response is not 200, nil is returned. "
   [{:keys [user-service-endpoint conn-timeout socket-timeout conn-mgr]} email]
   (let [url (str user-service-endpoint "/exp-account/tuids")
         resp (post-url url {:throw-exceptions true
@@ -74,7 +79,10 @@
       (accounts (:body resp))
       nil)))
 
-(defn- profile [r]
+(defn- profile
+  "take the raw xml response, parse it, and return a map containing the required
+   fields."
+  [r]
   (let [x (xml/parse (java.io.StringReader. r))
         z (zip/xml-zip x)]
     (if (= "true" (zx/xml1-> z (zx/attr :success)))
@@ -87,28 +95,28 @@
       (error (str "profile response not successful " r)))))
 
 (def exp-profile-template "<?xml version=\"1.0\" encoding=\"utf-8\"?><usr:getUserProfileRequest xmlns:usr=\"urn:com:expedia:www:services:user:messages:v3\" siteID=\"%s\"><usr:user actAsTuid=\"%s\" loggedInTuid=\"%s\"/><usr:messageInfo enableTraceLog=\"false\" clientName=\"localhost\" transactionGUID=\"a2192179-d5b7-4234-918c-8f662aaaf545\"/></usr:getUserProfileRequest>")
+
 ;"https://userservicev3.integration.karmalab.net:56783"
-(defn get-user-profile [{:keys [user-service-endpoint conn-timeout socket-timeout conn-mgr]} site-id tuid]
+(defn get-user-profile
+  "given http client and siteid tuid, return profile. An exception will be
+   thrown if there is a problem exchanging messages with user service.
+   If the xml response from user service indicates success = false,
+   nil is returned."
+  [{:keys [user-service-endpoint
+                                conn-timeout
+                                socket-timeout
+                                conn-mgr]} site-id tuid]
   (let [url (str user-service-endpoint "/profile/get")
         resp (post-url url {:throw-exceptions true
-                               :body (format exp-profile-template site-id tuid tuid)
-                               :content-type :xml
-                               :accept :xml
-                               :insecure? true
-                               :conn-timeout conn-timeout
-                               :socket-timeout socket-timeout
-                               :connection-manager conn-mgr } {:data {:siteid site-id
-                                                                      :tuid tuid}})]
+                            :body (format exp-profile-template site-id tuid tuid)
+                            :content-type :xml
+                            :accept :xml
+                            :insecure? true
+                            :conn-timeout conn-timeout
+                            :socket-timeout socket-timeout
+                            :connection-manager conn-mgr }
+                       {:data {:siteid site-id
+                        :tuid tuid}})]
     (if (= 200 (:status resp))
       (profile (:body resp))
       nil)))
-
-; curl -v -k -H "Content-Type: text/xml" -X POST -d '<?xml version="1.0" encoding="utf-8"?><usr:getUserProfileRequest xmlns:usr="urn:com:expedia:www:services:user:messages:v3"><usr:user actAsTuid="577015" loggedInTuid="577015"/><usr:pointOfSale tpid="1" eapid="0"/><usr:messageInfo enableTraceLog="false" clientName="localhost" transactionGUID="a2192179-d5b7-4234-918c-8f662aaaf545"/></usr:getUserProfileRequest>' 'https://userservicev3.integration.karmalab.net:56783/profile/get'
-;<?xml version="1.0" encoding="utf-8" standalone="yes"?><getUserProfileResponse success="true" xmlns="urn:com:expedia:www:services:user:messages:v3"><user passengerType="2" emailAddress="jmadynski@expedia.com" loginName="577015_EmailSignIn" hasGroups="false" locale="en_US"><personalName titleID="0" first="Elmer" middle="" last="Fudd"/><preferredPhone countryCode="1" phoneNumber="(415) 5050490"/><paymentInstrument paymentInstrumentID="75F85DBA-9274-4FC3-B7D6-E24BE4C5580C"><description>myvisa</description></paymentInstrument><paymentInstrument paymentInstrumentID="35DA0E90-A3DC-4C48-93AD-415B1B1D59E0"><description>otre Visa</description></paymentInstrument></user></getUserProfileResponse>
-;curl -v -k -H "Content-Type: text/xml" -X POST -d '<?xml version="1.0" encoding="utf-8"?><usr:listTUIDsForExpAccountRequest xmlns:usr="urn:com:expedia:www:services:user:messages:v3"><usr:expUser emailAddress="jmadynski@expedia.com"/><usr:pointOfSale tpid="1" eapid="0"/><usr:messageInfo enableTraceLog="false" clientName="localhost" transactionGUID="a2192179-d5b7-4234-918c-8f662aaaf545"/></usr:listTUIDsForExpAccountRequest>' 'https://userservicev3.integration.karmalab.net:56783/exp-account/tuids'
-;<?xml version="1.0" encoding="utf-8" standalone="yes"?><listTUIDsForExpAccountResponse success="true" xmlns="urn:com:expedia:www:services:user:messages:v3"><expUser id="301078" emailAddress="jmadynski@expedia.com"/><expUserTUIDMapping tpid="1" tuid="5363093" singleUse="true" updateDate="2014-04-25T09:55:00.000-07:00"/><expUserTUIDMapping tpid="1" tuid="577015" singleUse="false" updateDate="2014-05-30T22:26:00.000-07:00"/><authRealmID>1</authRealmID></listTUIDsForExpAccountResponse>
-
-
-
-;<?xml version="1.0" encoding="utf-8" standalone="yes"?><getUserProfileResponse success="false" xmlns="urn:com:expedia:www:services:user:messages:v3"><errorMessage errorCode="IllegalArguments">Invalid traveler (LoggedInTUID: 577015 or ActAsTUID: 577015).</errorMessage></getUserProfileResponse>
-;<?xml version="1.0" encoding="utf-8" standalone="yes"?><getUserProfileResponse success="true" xmlns="urn:com:expedia:www:services:user:messages:v3"><user passengerType="2" emailAddress="jmadynski@expedia.com" loginName="577015_EmailSignIn" hasGroups="false" locale="en_US"><personalName titleID="0" first="Elmer" middle="" last="Fudd"/><preferredPhone countryCode="1" phoneNumber="(415) 5050490"/><paymentInstrument paymentInstrumentID="75F85DBA-9274-4FC3-B7D6-E24BE4C5580C"><description>myvisa</description></paymentInstrument><paymentInstrument paymentInstrumentID="35DA0E90-A3DC-4C48-93AD-415B1B1D59E0"><description>otre Visa</description></paymentInstrument></user></getUserProfileResponse>

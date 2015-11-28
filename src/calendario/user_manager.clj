adaf9f4e65cc34a48f80f936ff07b33ea71406e5
Jeff Madynski <jmadynski@expedia.com>
2015-11-28 11:54:23 -0800

    reformat and add docs to methods

--- src/calendario/user_manager.clj
+++ src/calendario/user_manager.clj
@@ -2,10 +2,16 @@
   (:require [calendario.calusers :as cu]
             [calendario.user-service :as us]))
 
-(defn- add-siteuser! [db iduser uuid tpid tuid siteid locale now]
+(defn- add-siteuser!
+  "add a site user."
+  [db iduser uuid tpid tuid siteid locale now]
   (cu/create-site-user! db iduser uuid tpid 0 tuid siteid locale now))
 
-(defn create-user [db http-client siteid tuid]
+(defn create-user
+  "call user service to get profile, then call user service to get all
+   siteid/tuids for that email. Finally create the users in the
+   database."
+  [db http-client siteid tuid]
   (let [up (us/get-user-profile http-client siteid tuid)
         email (:email up)
         locale (:locale up)
@@ -15,11 +21,23 @@
                  db
                  (Integer/parseInt (:expuserid user-accounts)) email now)
         iduser (:iduser expuser)
-        siteusers (mapv #(add-siteuser! db iduser (java.util.UUID/randomUUID) (Integer/parseInt (:tpid %)) (Integer/parseInt (:tuid %)) siteid locale now) (:tuidmappings user-accounts))]
+        siteusers (mapv #(add-siteuser! db
+                                        iduser
+                                        (java.util.UUID/randomUUID)
+                                        (Integer/parseInt (:tpid %))
+                                        (Integer/parseInt (:tuid %))
+                                        siteid
+                                        locale
+                                        now)
+                        (:tuidmappings user-accounts))]
     { :expuser expuser
      :siteusers siteusers}))
 
-(defn user-lookup [db siteid tuid]
+(defn user-lookup
+  "take siteid / tuid and verify user exists in database. if user exists,
+   return map of user, expuser along with the siteusers. if user does not exist,
+   return nil."
+  [db siteid tuid]
   (if-let [su (cu/find-siteuser db siteid tuid)]
     (let [expuser (cu/find-expuser-by-siteid-tuid db siteid tuid)]
       {:expuser expuser
